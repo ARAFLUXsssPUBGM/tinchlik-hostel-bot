439499a777763bbacb6c6720b662021cba1ca74f
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-27 00:37:48 +0500

    Create keyboards.js

---
+++ keyboards/keyboards.js
@@ -0,0 +1,46 @@
+const mainKeyboard = {
+  keyboard: [
+    [{ text: "👤 Roʻyxatdan oʻtish" }],
+    [{ text: "🏨 HOSTEL bilan tanishish" }, { text: "📑 HOSTEL Qoidalar" }]
+  ],
+  resize_keyboard: true
+};
+
+const kvartirantKeyboard = {
+  keyboard: [
+    [{ text: "💰 Toʻlov qilish" }, { text: "📋 Mening profilim" }],
+    [{ text: "✍️ Adminga murojaat" }, { text: "📑 HOSTEL Qoidalar" }]
+  ],
+  resize_keyboard: true
+};
+
+const backKeyboard = {
+  keyboard: [[{ text: "⬅️ Ortga qaytish" }]],
+  resize_keyboard: true
+};
+
+const paymentTypeKeyboard = {
+  keyboard: [
+    [{ text: "💳 Karta orqali" }],
+    [{ text: "💵 Naqd pul bilan" }],
+    [{ text: "⬅️ Ortga qaytish" }]
+  ],
+  resize_keyboard: true
+};
+
+const adminMainKeyboard = {
+  keyboard: [
+    [{ text: "👥 Kvartirantlar" }, { text: "📊 Statistika" }],
+    [{ text: "⚙️ Hostel Sozlamalari" }, { text: "📢 Xabar yuborish" }],
+    [{ text: "⬅️ Chiqish" }]
+  ],
+  resize_keyboard: true
+};
+
+module.exports = {
+  mainKeyboard,
+  kvartirantKeyboard,
+  backKeyboard,
+  paymentTypeKeyboard,
+  adminMainKeyboard
+};
