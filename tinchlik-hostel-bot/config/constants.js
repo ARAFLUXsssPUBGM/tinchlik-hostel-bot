4c3a871ec6bc73bd5d15bbfbfcd2714dcff85fd8
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-28 15:47:20 +0500

    Add constants for database and session file paths

--- tinchlik-hostel-bot/config/constants.js
+++ tinchlik-hostel-bot/config/constants.js
@@ -1,1 +1,6 @@
-//.   Tizimdagi o'zgarmaslar
+const path = require('path');
+module.exports = {
+  MAIN_SUPER_ADMIN: parseInt(process.env.MAIN_SUPER_ADMIN, 10),
+  DB_FILE: path.join(__dirname, '..', 'core', 'database.json'),
+  SESSION_FILE: path.join(__dirname, '..', 'core', 'sessions.json')
+};
