542157b57d81c07bada5e8fcd3969789caa07bc9
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-29 01:31:52 +0500

    Refactor clearAndSend function for cleaner syntax

--- tinchlik-hostel-bot/utils/helpers.js
+++ tinchlik-hostel-bot/utils/helpers.js
@@ -2,22 +2,16 @@ const bot = require('../config/botConfig');
 const { sessions, saveSessions } = require('../core/session');
 
 async function clearAndSend(chatId, text, replyMarkup, isMainMenu = false) {
-  if (sessions[chatId] && sessions[chatId].lastMessageIds) {
-    for (let msgId of sessions[chatId].lastMessageIds) {
-      try { await bot.deleteMessage(chatId, msgId); } catch (e) {}
-    }
+  if (sessions[chatId]?.lastMessageIds) {
+    for (let msgId of sessions[chatId].lastMessageIds) { try { await bot.deleteMessage(chatId, msgId); } catch (e) {} }
     sessions[chatId].lastMessageIds = [];
-  } else {
-    if (!sessions[chatId]) sessions[chatId] = {};
-    sessions[chatId].lastMessageIds = [];
-  }
+  } else { sessions[chatId] = sessions[chatId] || {}; sessions[chatId].lastMessageIds = []; }
 
   try {
     const sentMsg = await bot.sendMessage(chatId, text, { reply_markup: replyMarkup, parse_mode: 'HTML' });
     if (isMainMenu) sessions[chatId].mainMenuBotMsgId = sentMsg.message_id;
     else sessions[chatId].lastMessageIds.push(sentMsg.message_id);
     saveSessions();
-  } catch (e) { console.error(`❌ Chat ${chatId} xato:`, e); }
+  } catch (e) {}
 }
-
 module.exports = { clearAndSend };
