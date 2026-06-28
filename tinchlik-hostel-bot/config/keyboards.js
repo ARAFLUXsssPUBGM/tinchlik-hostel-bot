6905b20606489e3b7b3eb157134e58239c6437e1
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-28 15:48:34 +0500

    Define main and admin keyboards for the bot

--- tinchlik-hostel-bot/config/keyboards.js
+++ tinchlik-hostel-bot/config/keyboards.js
@@ -1,1 +1,24 @@
-//.  ⌨️ Barcha asosiy klaviaturalar (mainKeyboard, adminMainKeyboard, genderKeyboard, paymentTypeKeyboard
+module.exports = {
+  mainKeyboard: {
+    keyboard: [[{ text: "👤 Roʻyxatdan oʻtish" }], [{ text: "🏨 HOSTEL bilan tanishish" }, { text: "🛂 HOSTEL Qoidalar" }]],
+    resize_keyboard: true
+  },
+  kvartirantKeyboard: {
+    keyboard: [[{ text: "📅 Ijara Muddati" }, { text: "💵 Toʻlov qilish" }], [{ text: "💳 Karta Raqam" }, { text: "📜 Qoidalar" }], [{ text: "🛂 Adminga murojat yoʻllash" }]],
+    resize_keyboard: true
+  },
+  adminMainKeyboard: {
+    keyboard: [
+      [{ text: "📊 STATISTIKA" }, { text: "📜 Qoida sozlash" }],
+      [{ text: "🏨 HOSTEL Sozlash" }, { text: "👮‍♂️ Admin qoʻshish" }],
+      [{ text: "💳 Karta Sozlamalari" }, { text: "📢 Xabarnoma" }],
+      [{ text: "🏨 HOSTEL tanishuv sozlamalari" }],
+      [{ text: "⛅ KUNLIK Toʻlovni sozlash" }],
+      [{ text: "⬅️ Ortga qaytish" }]
+    ],
+    resize_keyboard: true
+  },
+  backKeyboard: { keyboard: [[{ text: "⬅️ Ortga qaytish" }]], resize_keyboard: true },
+  genderKeyboard: { keyboard: [[{ text: "Erkak" }, { text: "Ayol" }], [{ text: "⬅️ Ortga qaytish" }]], resize_keyboard: true },
+  paymentTypeKeyboard: { keyboard: [[{ text: "💳 Karta orqali" }, { text: "💵 Naqd pul bilan" }], [{ text: "⬅️ Ortga qaytish" }]], resize_keyboard: true }
+};
