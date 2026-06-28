0deed41e4ca218fe788fe696cb44a13e5ac70c1c
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-28 23:45:55 +0500

    Implement start command for user session management

--- tinchlik-hostel-bot/handlers/commands/start.js
+++ tinchlik-hostel-bot/handlers/commands/start.js
@@ -1,1 +1,25 @@
-///start (Foydalanuvchi va aktiv/qarz kvartirantni ajratish)
+const bot = require('../../config/botConfig');
+const { db } = require('../../core/database');
+const { sessions } = require('../../core/session');
+const { pushState } = require('../../utils/navigation');
+const { clearAndSend } = require('../../utils/helpers');
+const { mainKeyboard, kvartirantKeyboard } = require('../../config/keyboards');
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+  if (!sessions[chatId]) sessions[chatId] = { history: [], lastMessageIds: [] };
+  sessions[chatId].history = [];
+
+  if (sessions[chatId].mainMenuUserMsgId) try { await bot.deleteMessage(chatId, sessions[chatId].mainMenuUserMsgId); } catch(e){}
+  if (sessions[chatId].mainMenuBotMsgId) try { await bot.deleteMessage(chatId, sessions[chatId].mainMenuBotMsgId); } catch(e){}
+  sessions[chatId].mainMenuUserMsgId = msg.message_id;
+
+  if (db.kvartirantlar[chatId] && ['aktiv', 'qarz'].includes(db.kvartirantlar[chatId].status)) {
+    const filial = db.kvartirantlar[chatId].filial || "HOSTEL";
+    pushState(chatId, 'KVARTIRANT_MENU');
+    await clearAndSend(chatId, `Assalomu alaykum <b>${filial}</b> Profilingizga xush kelibsiz...❕`, kvartirantKeyboard, true);
+  } else {
+    pushState(chatId, 'MAIN_MENU');
+    await clearAndSend(chatId, "<b>Tinchlik HOSTEL</b> tizimiga xush kelibsiz! Quyidagi tugmalardan birini tanlang:", mainKeyboard, true);
+  }
+});
