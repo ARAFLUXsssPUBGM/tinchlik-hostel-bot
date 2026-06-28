7c374b1e978981c76cda4cf6bd18b9b5b579b224
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-28 23:56:08 +0500

    Handle photo uploads for various user states

--- tinchlik-hostel-bot/handlers/photoRouter.js
+++ tinchlik-hostel-bot/handlers/photoRouter.js
@@ -1,1 +1,40 @@
 //.  bot.on('photo') -> Rasm (Selfi, Chek, Murojaat) yuklanishini boshqaruvchi
+const bot = require('../config/botConfig');
+const { sessions, saveSessions } = require('../core/session');
+const { pushState } = require('../utils/navigation');
+const { clearAndSend } = require('../utils/helpers');
+const { genderKeyboard, mainKeyboard, kvartirantKeyboard, backKeyboard } = require('../config/keyboards');
+const { sendRequestToAdmins, sendRenewRequestToAdmins, sendMurojaatToAdmins } = require('../services/adminNotifier');
+
+bot.on('photo', async (msg) => {
+  const chatId = msg.chat.id;
+  if (!sessions[chatId]) return;
+  const state = sessions[chatId].state;
+  const photoId = msg.photo[msg.photo.length - 1].file_id;
+
+  try { await bot.deleteMessage(chatId, msg.message_id); } catch(e){}
+
+  if (state === 'REG_SELFIE') {
+    sessions[chatId].regData.selfiePhoto = photoId;
+    pushState(chatId, 'REG_GENDER');
+    await clearAndSend(chatId, "7. Jinsingizni belgilang:", genderKeyboard);
+  }
+  else if (state === 'REG_SEND_CHEK') {
+    sessions[chatId].regData.chekPhoto = photoId;
+    await sendRequestToAdmins(chatId, true);
+    sessions[chatId].state = 'MAIN_MENU'; saveSessions();
+    await clearAndSend(chatId, "Soʻrovingiz Adminga yuborildi. Chek tekshirilib tasdiqlangach bot faollashadi.", mainKeyboard);
+  }
+  else if (state === 'KVAR_SEND_CHEK') {
+    sessions[chatId].renewData.chekPhoto = photoId;
+    await sendRenewRequestToAdmins(chatId, true);
+    sessions[chatId].state = 'KVARTIRANT_MENU'; saveSessions();
+    await clearAndSend(chatId, "To'lov skrinshoti adminga yetkazildi. Tekshiruvdan so'ng muddatingiz yangilanadi.", kvartirantKeyboard);
+  }
+  else if (state === 'KVAR_SEND_MUROJAAT') {
+    const captionText = msg.caption || "Matnsiz rasm ilova qilindi.";
+    await sendMurojaatToAdmins(chatId, photoId, captionText);
+    sessions[chatId].state = 'KVARTIRANT_MENU'; saveSessions();
+    await clearAndSend(chatId, "Murojaatingiz barcha adminlarga yuborildi!", kvartirantKeyboard);
+  }
+});
