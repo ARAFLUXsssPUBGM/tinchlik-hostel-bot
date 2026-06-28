38e8950b003050b9049666b1d737bf65d4104e06
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-29 02:24:40 +0500

    Implement appeal handling for tenant messages

--- tinchlik-hostel-bot/handlers/tenant/appeal.js
+++ tinchlik-hostel-bot/handlers/tenant/appeal.js
@@ -1,1 +1,17 @@
 //Adminga murojaat yo'llash (Matn + Rasm)
+const bot = require('../../config/botConfig');
+const { sessions, saveSessions } = require('../../core/session');
+const { clearAndSend } = require('../../utils/helpers');
+const { kvartirantKeyboard } = require('../../config/keyboards');
+const { sendMurojaatToAdmins } = require('../../services/adminNotifier');
+
+module.exports = async (msg, state) => {
+  const chatId = msg.chat.id;
+  try { await bot.deleteMessage(chatId, msg.message_id); } catch(e){}
+
+  if (state === 'KVAR_SEND_MUROJAAT' && msg.text) {
+    await sendMurojaatToAdmins(chatId, null, msg.text);
+    sessions[chatId].state = 'KVARTIRANT_MENU'; saveSessions();
+    return clearAndSend(chatId, "Murojaatingiz adminga yuborildi!", kvartirantKeyboard);
+  }
+};
