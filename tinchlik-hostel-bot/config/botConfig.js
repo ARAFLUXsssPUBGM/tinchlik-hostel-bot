30a188ee774ead2ab5fdb2958a013ac1ee0651e0
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-28 15:46:43 +0500

    Initialize Telegram bot with environment token

--- tinchlik-hostel-bot/config/botConfig.js
+++ tinchlik-hostel-bot/config/botConfig.js
@@ -1,1 +1,5 @@
-//.  .env dan ma'lumotlarni o'quvchi modul
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = process.env.TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+module.exports = bot;
