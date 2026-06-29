146cf3aeff2ee35ff92375f58ef5f47ada45268d
24 KOREN <167396104+ARAFLUXsssPUBGM@users.noreply.github.com>
2026-06-29 10:11:36 +0500

    Refactor start command to export as a function

--- tinchlik-hostel-bot/handlers/commands/start.js
+++ tinchlik-hostel-bot/handlers/commands/start.js
@@ -1,22 +1,26 @@
-const bot = require('../../config/botConfig');
 const { db } = require('../../core/database');
 const { sessions } = require('../../core/session');
 const { pushState } = require('../../utils/navigation');
 const { clearAndSend } = require('../../utils/helpers');
 const { mainKeyboard, kvartirantKeyboard } = require('../../config/keyboards');
 
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-  sessions[chatId] = sessions[chatId] || { history: [], lastMessageIds: [] }; sessions[chatId].history = [];
-  try { if (sessions[chatId].mainMenuUserMsgId) await bot.deleteMessage(chatId, sessions[chatId].mainMenuUserMsgId); } catch(e){}
-  try { if (sessions[chatId].mainMenuBotMsgId) await bot.deleteMessage(chatId, sessions[chatId].mainMenuBotMsgId); } catch(e){}
-  sessions[chatId].mainMenuUserMsgId = msg.message_id;
+// Fayl funksiya ko'rinishida eksport qilinmoqda, endi server.js (bot) ni bemalol uzata oladi
+module.exports = (bot) => {
+  bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id;
+    sessions[chatId] = sessions[chatId] || { history: [], lastMessageIds: [] }; 
+    sessions[chatId].history = [];
+    
+    try { if (sessions[chatId].mainMenuUserMsgId) await bot.deleteMessage(chatId, sessions[chatId].mainMenuUserMsgId); } catch(e){}
+    try { if (sessions[chatId].mainMenuBotMsgId) await bot.deleteMessage(chatId, sessions[chatId].mainMenuBotMsgId); } catch(e){}
+    sessions[chatId].mainMenuUserMsgId = msg.message_id;
 
-  if (db.kvartirantlar[chatId] && ['aktiv', 'qarz'].includes(db.kvartirantlar[chatId].status)) {
-    pushState(chatId, 'KVARTIRANT_MENU');
-    await clearAndSend(chatId, `Assalomu alaykum <b>${db.kvartirantlar[chatId].filial || "HOSTEL"}</b> Profilingizga xush kelibsiz!`, kvartirantKeyboard, true);
-  } else {
-    pushState(chatId, 'MAIN_MENU');
-    await clearAndSend(chatId, "<b>Tinchlik HOSTEL</b> tizimiga xush kelibsiz!", mainKeyboard, true);
-  }
-});
+    if (db.kvartirantlar[chatId] && ['aktiv', 'qarz'].includes(db.kvartirantlar[chatId].status)) {
+      pushState(chatId, 'KVARTIRANT_MENU');
+      await clearAndSend(chatId, `Assalomu alaykum <b>${db.kvartirantlar[chatId].filial || "HOSTEL"}</b> Profilingizga xush kelibsiz!`, kvartirantKeyboard, true);
+    } else {
+      pushState(chatId, 'MAIN_MENU');
+      await clearAndSend(chatId, "<b>Tinchlik HOSTEL</b> tizimiga xush kelibsiz!", mainKeyboard, true);
+    }
+  });
+};
